7a8d59302ab31cdb65374d881231a22591a51cfb
ValiantChip <egb154@gmail.com>
2025-07-29 18:09:41 -0400

    added functions

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -3,3 +3,28 @@ package pointer
 func New[T any](value T) *T {
 	return &value
 }
+
+func ZeroIfNil[T any](value *T) T {
+	if value == nil {
+		var zero T
+		return zero
+	}
+	return *value
+}
+
+func ValIfNil[T any](value *T, nValue T) T {
+	if value == nil {
+		return nValue
+	}
+	return *value
+}
+
+func Equals[T comparable](a, b *T) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return *a == *b
+}
