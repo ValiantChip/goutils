eb191a3522548f89476e3094fb70031f1efce8cb
ValiantChip <egb154@gmail.com>
2025-09-13 15:51:52 -0400

    added constants

--- rand/string.go
+++ rand/string.go
@@ -6,6 +6,13 @@ import (
 	"math/big"
 )
 
+const (
+	UPPER_CASE    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LOWER_CASE    = "abcdefghijklmnopqrstuvwxyz"
+	NUMERIC       = "0123456789"
+	ALPHA_NUMERIC = UPPER_CASE + LOWER_CASE + NUMERIC
+)
+
 // Returns a slice of bytes of the length provided using bytes uniformly selected from the provided dictionary
 func Bytes(reader io.Reader, dictionary []byte, length int64) ([]byte, error) {
 	b := make([]byte, length)
